318c6983e0f5df7fd64e0896ca30ea5635837dba
StreamAI <wang.baoliang@qq.com>
2019-12-13 19:17:11 +0800

    Add divide and conquer algorithm

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Data Structures and Algorithm in C/C++
 Data Structures and Algorithm analysis in C/C++
 
-## 1. 《[数据结构与算法分析（一）--- 递推与递归](https://blog.csdn.net/m0_37621078/article/details/103327986)》
+## 1. 《[数据结构与算法分析（一）--- 递推与递归 + 插入排序](https://blog.csdn.net/m0_37621078/article/details/103327986)》
 
-
+## 2.《[数据结构与算法分析（二）--- 分治与减治 + 快速排序](https://blog.csdn.net/m0_37621078/article/details/103468599)》
 
--- algorithm/sort.c
+++ algorithm/sort.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-#define MAX_COUNT      10000
-#define SHOW_COUNT     100
+#define MAX_COUNT      1000000
+#define SHOW_COUNT     30
 
 int *data = NULL;
 
@@ -21,16 +22,51 @@ int * data_init(int *data, int n)
     return data;
 }
 
-void show_data(int *data, int n)
+void show_data(int *data, int n, int m)
 {
     int i;
 
     printf("data show:\n");
-    for(i = 0; i < n; i++)
+    for(i = 0; i < m; i++)
     {
         printf("%d  ", data[i]);
     }
+
+    printf("\n......\n");   
+    for(i = n - m; i < n; i++)
+    {
+        printf("%d  ", data[i]);
+    }
+
     printf("\n\n");
+}
+
+bool validate_data(int *data, int n)
+{
+    bool res = true;
+    int i;
+    for (i = 1; i < n; i++)
+    {
+        if(data[i] < data[i - 1])
+            res = false;
+    }
+
+    if(res == true)
+        printf("\nThe data sequence has been ordered.\n\n");
+    else
+        printf("\nThe data sequence is unordered.\n\n");
+
+    return res;
+}
+
+void swap_data(int *a, int *b)
+{
+    if(*a != *b)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
 }
 
 void insert_sort(int *data, int n, int k, int step)
@@ -46,7 +82,7 @@ void insert_sort(int *data, int n, int k, int step)
     }
     data[i + step] = temp; 
 
-    insert_sort(data, n, k+ step, step);
+    insert_sort(data, n, k + step, step);
 }
 
 void shell_sort(int *data, int n, int step)
@@ -61,6 +97,112 @@ void shell_sort(int *data, int n, int step)
     shell_sort(data, n, step / 2);
 }
 
+void merge_data(int *data, int *temp, int left, int mid, int right)
+{
+    int i = left, j = mid + 1, k = 0;
+
+    while(i <= mid && j <= right)
+    {
+        if(data[i] <= data[j])
+            temp[k++] = data[i++];
+        else
+            temp[k++] = data[j++];
+    }
+
+    while(i <= mid)
+        temp[k++] = data[i++];
+
+    while(j <= right)
+        temp[k++] = data[j++];
+
+    for(i = left, k = 0; i <= right; i++, k++)
+        data[i] = temp[k];
+}
+
+void recursive_merge(int *data, int *temp, int left, int right)
+{
+    if(left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    recursive_merge(data, temp, left, mid);
+    recursive_merge(data, temp, mid + 1, right);
+
+    merge_data(data, temp, left, mid, right);
+}
+
+void merge_sort(int *data, int n)
+{
+    int *temp = malloc(n * sizeof(int));
+
+    recursive_merge(data, temp, 0, n - 1);
+
+    free(temp);
+}
+
+int partition(int *data, int left, int right)
+{
+    /*
+    // 随机选择分界线或枢纽值
+    int index = left + rand() % (right - left + 1);
+    swap_data(&data[index], &data[right]);
+    int i = left, j = right - 1, pivot = right;
+    */
+    // 三数取中法选择分界线或枢纽值
+    int mid = left + (right - left) / 2;
+    if(data[left] > data[mid])
+        swap_data(&data[left], &data[mid]);
+    if(data[left] > data[right])
+        swap_data(&data[left], &data[right]);
+    if(data[mid] > data[right])
+        swap_data(&data[mid], &data[right]);
+
+    swap_data(&data[mid], &data[right-1]);
+    int i = left + 1, j = right - 2, pivot = right - 1;
+    
+    while (true)
+    {
+        while (data[i] < data[pivot])
+            i++;
+
+        while (j > left && data[j] >= data[pivot])
+            j--;
+        
+        if(i < j)
+            swap_data(&data[i], &data[j]);
+        else
+            break;
+    }
+    
+    if(i < right)
+        swap_data(&data[i], &data[pivot]);
+
+    return i;
+}
+
+void quick_sort(int *data, int left, int right)
+{
+    if(right - left <= 1)
+    {
+        if(right - left == 1 && data[left] > data[right])
+            swap_data(&data[left], &data[right]);
+  
+        return;
+    }
+
+    int divide = partition(data, left, right);
+    quick_sort(data, left, divide - 1);
+    quick_sort(data, divide + 1, right);
+}
+
+void quicksort(int *data, int n)
+{
+    if(n < 15)
+        insert_sort(data, n, 1, 1);
+    else
+        quick_sort(data, 0, n - 1);
+}
+
 int main(void)
 {
     int k, n;
@@ -69,6 +211,8 @@ int main(void)
 
     printf("insert sort: 1\n");
     printf("shell  sort: 2\n");
+    printf("merge  sort: 3\n");
+    printf("quick  sort: 4\n");
     printf("Select method: ");
     scanf("%d", &k);
 
@@ -84,6 +228,14 @@ int main(void)
     case 2:
         shell_sort(data, MAX_COUNT, MAX_COUNT / 2);
         break;
+
+    case 3:
+        merge_sort(data, MAX_COUNT);
+        break;
+
+    case 4:
+        quicksort(data, MAX_COUNT);
+        break;
     
     default:
         break;
@@ -91,8 +243,9 @@ int main(void)
     end = clock();
     time = (double)(end - start) / (CLOCKS_PER_SEC / 1000);
 
-    show_data(data, SHOW_COUNT);
-    printf("execution time: %.3lf ms.\n", time);
+    //show_data(data, MAX_COUNT, SHOW_COUNT);
+    validate_data(data, MAX_COUNT);
+    printf("execution time: %.3lf ms.\n\n", time);
 
     free(data);
     return 0;
